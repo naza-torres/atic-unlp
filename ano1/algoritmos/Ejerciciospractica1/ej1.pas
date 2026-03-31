dc76d54428e097d70b5ded82c69031dead39633b
Nazareno Torres <brunogastonbosch@gmail.com>
2026-03-31 17:04:53 -0300

    CADP P1: ej1 - comparar dos enteros

---
+++ ano1/algoritmos/Ejerciciospractica1/ej1.pas
@@ -0,0 +1,19 @@
+
+
+Program uno;
+
+Var 
+  num1: Integer;
+  num2: integer;
+Begin
+  Read(num1);
+  Read(num2);
+  If (num1 > num2) Then
+    Write(num1)
+
+  Else If (num2 > num1)
+         Then
+         write(num2)
+  Else
+    write('los numeros son iguales')
+End.
